233b71f6de4c4307abb92858156ada92158f2cd7
Alex Paul <apaul@planet.com>
2019-05-02 00:14:57 -0700

    WIP

--- arduino/sensors/sensors.ino
+++ arduino/sensors/sensors.ino
@@ -1,147 +1,159 @@
-  
- /*
- The circuit:
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 8
- * LCD D4 pin to digital pin 9
- * LCD D5 pin to digital pin 10
- * LCD D6 pin to digital pin 11
- * LCD D7 pin to digital pin 12
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+#include <Wire.h> 
+//#include <LiquidCrystal_I2C.h>
+#include <AccelStepper.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
+// OLED display TWI address
+#define OLED_ADDR   0x3C
 
- This example code is in the public domain.
+// reset pin not used on 4-pin OLED module
+Adafruit_SSD1306 display(-1);  // -1 = no reset pin
 
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
-
-// include the library code:
-#include <LiquidCrystal.h>
-
-#define VCC -1
-#define V33 -3
-#define NC 0
-#define GND -2
-
-#define PIN_LCD_VSS GND
-#define PIN_LCD_VDD 53
-// VO: Contrast adjustment (0V-VDD)
-#define PIN_LCD_VO 51
-// RS: Register selection H: Data register (for read and write) L: Instruction code (for write)
-#define PIN_LCD_RS 49
-// RW: H: Read (Host module) L: write(Host Module)
-#define PIN_LCD_RW 47
-// E: Read/write enable signal. H: Read data is enabled by a high level. H->L: Write data is latched on the falling edge.
-#define PIN_LCD_E  45
-// D0 - D7: data bit 0 - 7
-#define PIN_LCD_D0 43
-#define PIN_LCD_D1 41
-#define PIN_LCD_D2 39
-#define PIN_LCD_D3 37
-#define PIN_LCD_D4 35
-#define PIN_LCD_D5 33
-#define PIN_LCD_D6 31
-#define PIN_LCD_D7 29
-// A (LED +): Supply voltage for LED. “A”(anode)or “+” of LED backlight.
-#define PIN_LCD_A  27
-// K (LED -): Supply voltage for LED. “K”(cathode or cathode for German and original) Greek spelling) or “-” of LED backlight.
-#define PIN_LCD_K  25
-
-#define LCD_W 16
-#define LCD_H 2
-
-//  LiquidCrystal(uint8_t rs, uint8_t enable,
-//      uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-//      uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
-//  LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
-//      uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
-//      uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
-// LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
-//      uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3)
-// LiquidCrystal(uint8_t rs, uint8_t enable,
-//      uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
-
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(PIN_LCD_RS, PIN_LCD_RW, PIN_LCD_E, 
-    PIN_LCD_D0, PIN_LCD_D1, PIN_LCD_D2, PIN_LCD_D3,
-    PIN_LCD_D4, PIN_LCD_D5, PIN_LCD_D6, PIN_LCD_D7);
+// 128 x 64 pixel display
+//#if (SSD1306_LCDHEIGHT != 64)
+//#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+//#endif
 
 void setup() {
-    //pinMode(PIN_LCD_VSS, OUTPUT);
-    pinMode(PIN_LCD_VDD, OUTPUT);
-    pinMode(PIN_LCD_A, OUTPUT);
-    pinMode(PIN_LCD_K, OUTPUT);
-    pinMode(PIN_LCD_VO, OUTPUT);
+  // initialize and clear display
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.display();
 
-    //digitalWrite(PIN_LCD_VSS, LOW);
-    digitalWrite(PIN_LCD_VDD, HIGH);
-    digitalWrite(PIN_LCD_A, HIGH);
-    digitalWrite(PIN_LCD_K, LOW);
-    digitalWrite(PIN_LCD_VO, HIGH);
-
-  // set up the LCD's number of columns and rows:
-  lcd.begin(LCD_W, LCD_H);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-  lcd.display();
-
-  //blink(PIN_LCD_VO);
-  //blink(PIN_LCD_A);
-  //blink(PIN_LCD_VDD);
-}
-
-void blink(int pin) {
-    int i;
-    for (i = 0; i < 4; i++) {
-        digitalWrite(pin, LOW);
-        delay(500);
-        digitalWrite(pin, HIGH);
-        delay(500);
-    }
+  
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  //lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  //lcd.print(millis() / 1000);
-  //lcd.display();
-  delay(500);
+  // put your main code here, to run repeatedly:
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  char buf[128];
+  sprintf(buf, "up %09u ms", millis());
+  display.print(buf);
+  display.display();
 }
 
+
+#define PIN_LCD_SDA 20
+#define PIN_LCD_SCL 21
+#define PIN_US_TRIGGER 12
+#define PIN_US_ECHO 11
+#define US_SLOPE 5.9
+#define US_INTERCEPT 0
+
+#define STEPPER_STEPS_PER_REV 200
+#define MAX_SPEED_STEPS_PER_SEC (0.25 * STEPPER_STEPS_PER_REV)
+#define H_STEPPER_PIN_ENABLE 51
+#define H_STEPPER_PIN_DIR 52
+#define H_STEPPER_PIN_PUL 53
+
+#define DRIVE_MS 500
+
 /*
-ultrasonic
-#define TRIGGER 12
-#define ECHO 11
+
+AccelStepper height_stepper(AccelStepper::DRIVER,
+  H_STEPPER_PIN_PUL, H_STEPPER_PIN_DIR);
+
+
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
-pinMode(ECHO, INPUT);
-pinMode(TRIGGER, OUTPUT);
-digitalWrite(TRIGGER, LOW);
-Serial.begin(9600);
+    setup_ultrasonic();
+    setup_lcd();
+    height_stepper.setMaxSpeed(MAX_SPEED_STEPS_PER_SEC);
+    height_stepper.setSpeed(MAX_SPEED_STEPS_PER_SEC);
+    height_stepper.setAcceleration(MAX_SPEED_STEPS_PER_SEC / 2);
+    //height_stepper.setPinsInverted(directionInvert = false, stepInvert = false, enableInvert = false);
+    //height_stepper.setPinsInverted(true, false, true);
+    
+    //long d = 1000;
+    //refresh_disp(d);
+    //height_stepper.runToNewPosition(d);
+    //height_stepper.moveTo(1000);
+
+    //while (height_stepper.runSpeed()) {
+    //    refresh_disp(0);
+    //}
+    //height_stepper.moveTo(0);
+    //while (height_stepper.runSpeed()) {
+        //wait
+    //}
+    //refresh_disp(0);
 }
-void loop()
+
+void setup_lcd() {
+
+  lcd.init();  //initialize the lcd
+  lcd.backlight();  //open the backlight
+  
+  
+}
+
+void refresh_disp(long d) {
+    char buf[20];
+    lcd.setCursor ( 0, 0 );
+    sprintf(buf, "Up %09u ms", millis());
+    lcd.print(buf);
+    lcd.setCursor (0, 1);
+    sprintf(buf, "us: %09u mm", d);
+    lcd.print(buf);
+    lcd.setCursor (0, 2);
+    sprintf(buf, "pos: %09d steps", height_stepper.currentPosition());
+    lcd.print(buf);
+}
+
+
+void loop() 
 {
-long distance, duration;
-digitalWrite(TRIGGER, HIGH);
-delayMicroseconds(10);
-digitalWrite(TRIGGER, LOW);
-duration = pulseIn(ECHO,HIGH);
-distance = duration / 2 / 7.6;
-Serial.println(distance);
-}*/
+    long d = get_us_distance();
+    refresh_disp(d);   
+    
+    height_stepper.moveTo(d);
+    //height_stepper.setSpeed(MAX_SPEED_STEPS_PER_SEC);
+    
+    //height_stepper.runSpeedToPosition();
+
+    long start = millis();
+    while (millis() < start + DRIVE_MS && millis() > start - 1) {
+        height_stepper.run();
+    }
+    //    d = get_us_distance();
+    //    refresh_disp(d);  
+    //}
+    //delayMicroseconds(10);
+    //delay(500);
+    /*lcd.setCursor ( 0, 1 );
+    lcd.print(d);
+    lcd.setCursor ( 0, 2 );
+    lcd.print("  20 cols, 4 rows   ");
+    lcd.setCursor ( 0, 3 );
+    lcd.print(" www.sunfounder.com ");
+}
+
+// * /
+
+long get_us_distance() {
+    unsigned long distance, duration;
+    digitalWrite(PIN_US_TRIGGER, HIGH);
+    long s = micros();
+    while (micros() < s + 10) {
+        height_stepper.runSpeed();
+    }
+    digitalWrite(PIN_US_TRIGGER, LOW);
+    duration = pulseIn(PIN_US_ECHO,HIGH);
+    distance = duration;
+    return distance / US_SLOPE + US_INTERCEPT;
+}
+
+void setup_ultrasonic() {
+    pinMode(PIN_US_ECHO, INPUT);
+    pinMode(PIN_US_TRIGGER, OUTPUT);
+    digitalWrite(PIN_US_TRIGGER, LOW);
+}
+
+// */
